7df27181f9fcc4f394e6759663aca020ba109a8e
naimteb <tebchranynaim@gmail.com>
2025-04-03 09:30:37 +0300

    updates

--- tic.js
+++ tic.js
@@ -2,6 +2,7 @@ let playertxt = document.getElementById("playertxt");
 let restartbtn = document.getElementById("restartBtn");
 let boxes = Array.from(document.getElementsByClassName("box"));
 
+let flag = false;
 const X_txt = "X";
 const O_txt = "O";
 let current_player = X_txt;
@@ -11,12 +12,16 @@ const startgame = () => {
   boxes.forEach((box) => box.addEventListener("click", boxclicked));
 };
 function boxclicked(e) {
-  const id = e.target.id;
+  if (flag) {
+    return;
+  }
+  const id = Number(e.target.id);
   if (spaces[id] == null) {
     spaces[id] = current_player;
     e.target.innerText = current_player;
     if (playerhaswon()) {
       playertxt.innerText = `${current_player} has won!`;
+      flag = true;
 
       return;
     }
@@ -31,7 +36,7 @@ function restart() {
     box.innerText = "";
   });
   current_player = X_txt;
-  playertxt = "Tic-Tac-toe";
+  playertxt.innerText = "Tic-Tac-toe";
 }
 
 const wining_loc = [
@@ -44,14 +49,24 @@ const wining_loc = [
   [0, 4, 8],
   [2, 4, 6],
 ];
+// function playerhaswon() {
+//   for (let object of wining_loc) {
+//     let [a, b, c] = object;
+//     if (spaces[a] && spaces[a] == spaces[b] && spaces[a] == spaces[c]) {
+//       return [a, b, c];
+//     }
+//   }
+//   return false;
+// }
 function playerhaswon() {
-  for (let object of wining_loc) {
-    let [a, b, c] = object;
+  for (let index = 0; index < wining_loc.length; index++) {
+    let a = wining_loc[index][0];
+    let b = wining_loc[index][1];
+    let c = wining_loc[index][2];
     if (spaces[a] && spaces[a] == spaces[b] && spaces[a] == spaces[c]) {
       return [a, b, c];
     }
   }
   return false;
 }
-
 startgame();
